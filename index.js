7a19fce11a0fab7dd2ab0319b7e58ffe36616e85
Terence Ma <tma@gsma.com>
2018-08-06 16:49:02 +0800

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); // generates a new application, may have multiple
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ bye: 'buddy' });
 });
 
 const PORT = process.env.PORT || 5000;
