7fb55e2c26f2f2af7cd525205fc0ace69d33197d
Terence Ma <tma@gsma.com>
2018-08-14 10:17:01 +0800

    Added billing and client side app!

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,3 +6,9 @@ export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user'); // relative path is used here -- proxy is used in dev
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+export const handleToken = token => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
+  // assumes the back-end with send back an updated user model with the correct number of credits so same dispatch
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -13,6 +14,7 @@ mongoose.connect(
 
 const app = express(); // generates a new application, may have multiple
 
+app.use(bodyParser.json());
 //-- app.use() are wiring up middleware before sent to router handlers
 app.use(
   cookieSession({
@@ -24,7 +26,20 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app); // require statement returns the function then inserting the parameter executes it
+require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
